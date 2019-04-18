9cd253ee769ebdabf86c7cf211ae7d9eabf1a368
ludovic2411 <ludovic.ceccotti@gmail.com>
2019-04-18 14:44:34 +0200

    add cors

--- src/main/java/com/example/demo/controllers/CategoryController.java
+++ src/main/java/com/example/demo/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dal.entities.Category;
 import com.example.demo.services.CategoryService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
